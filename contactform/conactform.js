c4fb7f3f1188add4def50dd2ff99b5170b7bd018
abanoub-hanna-kamel <abanobbuffon@gmail.com>
2020-05-23 05:22:40 -0700

    first commit

--- contactform/conactform.js
+++ contactform/conactform.js
@@ -1,118 +1,75 @@
-jQuery(document).ready(function($) {
-  "use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+ const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  //Contact
-  $('form.contactForm').submit(function() {
-    var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html")
 
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
+})
 
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
+app.post("/", function (req, res) {
 
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
+const firstname = req.body.aaa;
+const email = req.body.aaaa;
+const subject = req.body.aaaaa;
+const message = req.body.aaaaaa;
+ const data = {
+   members: [
+     {
+       email_address: email,
+       status: "subscribed",
+       merge_fields: {
+         FNAME: firstname,
+         LNAME: subject,
+       }
+     }
+   ]
+ };
 
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
+const jsonData = JSON.stringify(data);
 
-          case 'email':
-            if (!emailExp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
+const url = "https://us18.api.mailchimp.com/3.0/lists/16437decac";
 
-          case 'checked':
-            if (! i.is(':checked')) {
-              ferror = ierror = true;
-            }
-            break;
+const options = {
+  method: "POST" ,
+  auth: "abnaoub1:f08714f57073f264239f5c3df047c743-us18"
+}
 
-          case 'regexp':
-            exp = new RegExp(exp);
-            if (!exp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
-    });
-    f.children('textarea').each(function() { // run all inputs
+const request = https.request(url, options, function (response) {
 
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
+  if (response.statuscode === 200) {
+    res.send("yes");
+  }else {
+    res.send("no");
+  }
 
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
+  response.on("data", function (data) {
+    console.log(JSON.parse(data));
 
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
+  })
 
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
-    });
-    if (ferror) return false;
-    else var str = $(this).serialize();
-    var action = $(this).attr('action');
-    if( ! action ) {
-      action = 'contactform/contactform.php';
-    }
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
+})
 
-      }
-    });
-    return false;
-  });
+request.write(jsonData);
+request.end();
+
 
 });
+
+app.listen(process.env.PORT || 3000, function functionName() {
+  console.log("server run");
+
+});
+
+
+
+
+
+
+////f08714f57073f264239f5c3df047c743-us18
+//listid   16437decac
